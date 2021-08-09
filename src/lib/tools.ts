591f066840ceeaaafb61c0834e974116898b8fd2
Andy.Bin <andy.bin@crypto.com>
2021-08-09 11:35:35 +0800

    🎨 remove all query params in cwe bridge url

--- src/lib/tools.ts
+++ src/lib/tools.ts
@@ -2,11 +2,22 @@ import { IParseURIResult } from '@walletconnect/types'
 import { appendToQueryString, getQueryString, parseWalletConnectUri } from '@walletconnect/utils'
 
 export const formatToCWEURI = (uri: string) => {
-  return uri.replace('wc:', 'CWE:')
+  const result: IParseURIResult = parseWalletConnectUri(uri)
+  const bridge = encodeURIComponent(removeAllUrlParams(decodeURIComponent(result.bridge)))
+  return `CWE:${result.handshakeTopic}@${result.version}?bridge=${bridge}&key=${result.key}`
 }
 
 export const formatToWCURI = (uri: string) => {
-  return uri.replace('CWE:', 'wc:')
+  const result: IParseURIResult = parseWalletConnectUri(uri)
+  return `wc:${result.handshakeTopic}@${result.version}?bridge=${result.bridge}&key=${result.key}`
+}
+
+export const removeAllUrlParams = (url: string): string => {
+  if (!url || url.length === 0) {
+    return url
+  }
+  const splitUrl = url.split('?')
+  return splitUrl[0]
 }
 
 export const addUrlParams = (url: string, params: any): string => {
