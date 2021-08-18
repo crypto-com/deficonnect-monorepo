3d777457624459bb179e14427c8aa7371e16903e
Andy.Bin <andy.bin@crypto.com>
2021-08-18 17:35:00 +0800

    ✨support custom the key of storage api

--- src/lib/SessionStorage.ts
+++ src/lib/SessionStorage.ts
@@ -1,23 +1,28 @@
 import { ISessionStorage, IWalletConnectSession } from '@walletconnect/types'
 import { isWalletConnectSession, getLocal, setLocal, removeLocal } from '@walletconnect/utils'
 
-const key = 'cc_walletconnect_session_storage'
+const defaultKey = 'DeFiLink_session_storage_dapp'
 
 export class SessionStorage implements ISessionStorage {
-  getSession(): IWalletConnectSession | null {
-    const session = getLocal(key)
+  key: string
+  constructor({ key } = { key: defaultKey }) {
+    this.key = key
+  }
+
+  getSession = (): IWalletConnectSession | null => {
+    const session = getLocal(this.key)
     if (session && isWalletConnectSession(session) && session.accounts && session.accounts.length > 0) {
       return session
     }
     return null
   }
 
-  setSession(session: IWalletConnectSession): IWalletConnectSession {
-    setLocal(key, session)
+  setSession = (session: IWalletConnectSession): IWalletConnectSession => {
+    setLocal(this.key, session)
     return session
   }
 
-  removeSession() {
-    removeLocal(key)
+  removeSession = () => {
+    removeLocal(this.key)
   }
 }
