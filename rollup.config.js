f576413d9647f4941b4d088db440c3f3c5e57d93
Andy Bin <andy.bin@crypto.com>
2022-03-11 22:03:21 +0800

    feat: add rollup for UMD bundle

---
+++ rollup.config.js
@@ -0,0 +1,66 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+import typescript from '@rollup/plugin-typescript'
+import json from '@rollup/plugin-json'
+// import builtins from 'rollup-plugin-node-builtins'
+import globals from 'rollup-plugin-node-globals'
+// import auto from '@rollup/plugin-auto-install'
+import nodePolyfills from 'rollup-plugin-node-polyfills'
+import inject from 'rollup-plugin-inject'
+
+// `npm run build` -> `production` is true
+// `npm run dev` -> `production` is false
+const production = !process.env.ROLLUP_WATCH
+
+const defaultConfige = {
+  plugins: [
+    typescript({
+      target: 'es6',
+    }),
+    json(),
+    resolve(), // tells Rollup how to find date-fns in node_modules
+    commonjs(), // converts date-fns to ES modules
+    production && terser(), // minify, but only in production
+  ],
+}
+
+const configes = [
+  {
+    ...defaultConfige,
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.umd.js',
+      name: 'DeFiConnect',
+      format: 'umd', // immediately-invoked function expression — suitable for <script> tags
+      sourcemap: true,
+    },
+    plugins: [
+      commonjs(), // converts date-fns to ES modules
+      // auto({ manager: 'npm' }),
+      nodePolyfills(),
+      resolve(), // tells Rollup how to find date-fns in node_modules
+      // globals({
+      //   global: {
+      //     EventEmitter: require('events'),
+      //   },
+      // }),
+      // inject({
+      //   EventEmitter2: 'dasdas',
+      //   EventEmitter: require('events'),
+      //   // modules: {
+      //   //   EventEmitter: 'events',
+      //   // },
+      // }),
+      // builtins(),
+      typescript({
+        tsconfig: 'tsconfig.json',
+      }),
+      json(),
+      // production && terser(), // minify, but only in production
+    ],
+    // external: ['ws'],
+  },
+]
+
+export default configes
