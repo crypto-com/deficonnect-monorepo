98afbeb9775a0a0d576b5b3913695f81dde89e93
Jack Feng <jack.feng@crypto.com>
2022-08-15 19:10:36 +0800

    chore: update qrcode modal provider UI

---
+++ packages/qrcode-modal/src/icons/defi-main-icon-mobile.tsx
@@ -0,0 +1,45 @@
+import { h } from "preact"
+
+function DeFiMainIconMobile(){
+  return (
+    <svg width="197" height="102" viewBox="0 0 197 102" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g filter="url(#filter0_d_8224_39097)">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M107.254 22.9492C112.85 22.9492 115.647 22.9492 118.66 23.9076C121.949 25.1123 124.539 27.7191 125.736 31.029C126.688 34.0603 126.688 36.8765 126.688 42.508V59.4904C126.688 65.1224 126.688 67.9377 125.736 70.9694C124.539 74.2793 121.949 76.8861 118.66 78.0904C115.647 79.0492 112.85 79.0492 107.254 79.0492H90.3796C84.7835 79.0492 81.9856 79.0492 78.9737 78.0904C75.6848 76.8861 73.0947 74.2793 71.8976 70.9694C70.9453 67.9377 70.9453 65.1224 70.9453 59.4904V42.508C70.9453 36.8765 70.9453 34.0603 71.8976 31.029C73.0947 27.7191 75.6848 25.1123 78.9737 23.9076C81.9856 22.9492 84.7835 22.9492 90.3796 22.9492H107.254Z" fill="white"/>
+      </g>
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M98.7902 29.2188L79.8136 40.1133V61.9036L98.7902 72.7995L117.767 61.9036V40.1133L98.7902 29.2188Z" fill="#FEFEFE"/>
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M98.7902 29.2188L79.8136 40.1133V61.9036L98.7902 72.7995L117.767 61.9036V40.1133L98.7902 29.2188ZM91.2978 38.6389H106.232L108.032 46.2103H89.5651L91.2978 38.6389ZM95.6007 51.5828L93.972 47.354H103.643L102.048 51.5828L102.511 56.3069L98.7902 56.324H95.1036L95.6007 51.5828ZM100.351 61.3204V59.8204L103.694 56.6315V51.5998L108.067 48.7697L113.057 52.5042L106.265 64.2712H103.574L100.351 61.3204ZM84.5249 52.5042L89.5306 48.8037L93.9548 51.5998V56.6315L97.2978 59.8204V61.3204L94.0752 64.3054H91.3484L84.5249 52.5042Z" fill="#002D72"/>
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M106.265 64.2712H103.574L100.351 61.3204V59.8204L103.694 56.6315V51.5998L108.067 48.7697L113.057 52.5042L106.265 64.2712ZM98.79 29.2188V38.6391H106.232L108.032 46.2105H98.79V47.3542H103.643L102.048 51.5828L102.511 56.3069L98.79 56.324V72.7995L117.767 61.9038V40.1133L98.79 29.2188Z" fill="url(#paint0_linear_8224_39097)" style="mix-blend-mode:multiply"/>
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M97.2979 61.3204L94.0751 64.3054H91.3483L84.5248 52.5042L89.5307 48.8039L93.9549 51.5998V56.6315L97.2979 59.8204V61.3204ZM95.1034 56.324L95.6005 51.5828L93.972 47.3542H98.7901V46.2105H89.565L91.2979 38.6391H98.7901V29.2188L79.8135 40.1133V61.9038L98.7901 72.7995V56.324H95.1034Z" fill="url(#paint1_linear_8224_39097)" style="mix-blend-mode:multiply"/>
+      <ellipse cx="2.06611" cy="46.0758" rx="1.90032" ry="1.9125" transform="rotate(-180 2.06611 46.0758)" fill="#1199FA"/>
+      <ellipse cx="158.993" cy="87.9742" rx="1.90032" ry="1.9125" transform="rotate(-180 158.993 87.9742)" fill="#F76341"/>
+      <ellipse cx="134.455" cy="11.0133" rx="1.90032" ry="1.9125" transform="rotate(-180 134.455 11.0133)" fill="#FFBF00"/>
+      <ellipse cx="168.178" cy="65.9805" rx="1.5836" ry="1.59375" transform="rotate(-180 168.178 65.9805)" fill="#1199FA"/>
+      <ellipse cx="164.144" cy="17.9379" rx="1.26688" ry="1.275" transform="rotate(-180 164.144 17.9379)" fill="#93D2FD"/>
+      <ellipse cx="192.883" cy="36.657" rx="0.950165" ry="0.95625" transform="rotate(-180 192.883 36.657)" fill="#E64B60"/>
+      <ellipse cx="39.9896" cy="5.82656" rx="1.26688" ry="1.275" transform="rotate(-180 39.9896 5.82656)" fill="#FFBF00"/>
+      <ellipse cx="26.2877" cy="83.832" rx="1.5836" ry="1.59375" transform="rotate(-180 26.2877 83.832)" fill="#20BCA4"/>
+      <ellipse cx="30.4051" cy="58.0121" rx="1.26688" ry="1.275" transform="rotate(-180 30.4051 58.0121)" fill="#93D2FD"/>
+      <defs>
+        <filter id="filter0_d_8224_39097" x="50.9453" y="2.94922" width="95.7428" height="96.0996" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+          <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+          <feOffset/>
+          <feGaussianBlur stdDeviation="10"/>
+          <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0"/>
+          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_8224_39097"/>
+          <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_8224_39097" result="shape"/>
+        </filter>
+        <linearGradient id="paint0_linear_8224_39097" x1="115.743" y1="68.098" x2="115.743" y2="33.8127" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#002D72"/>
+          <stop offset="1" stop-color="#002D72" stop-opacity="0.01"/>
+        </linearGradient>
+        <linearGradient id="paint1_linear_8224_39097" x1="96.7663" y1="68.098" x2="96.7663" y2="33.8127" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#002D72" stop-opacity="0.01"/>
+          <stop offset="1" stop-color="#002D72"/>
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
+
+export default DeFiMainIconMobile
